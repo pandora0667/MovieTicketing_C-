837f378845c3064522324c1659f16a8eb70ec3d9
swLEE <jusk2@naver.com>
2016-12-04 02:43:34 +0900

    Error LOOP

--- Movie_Ticketing_System.cpp
+++ Movie_Ticketing_System.cpp
@@ -43,7 +43,7 @@ public :
 	}
 	void getMovieInformation()
 	{
-		return  movieTitle, movieRelease, moviePrice << endl; 
+		 cout <<  movieTitle << ", " <<  movieRelease << ", " <<  moviePrice << endl; 
 	}
 	~movieInformation(){
 	}
